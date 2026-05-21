52cb2a26444333407ad687224904de57f2dfcd8f
gwhaller <gwhaller@gmail.com>
2026-05-21 12:19:28 +0200

    fixed app

--- app/quasar.config.js
+++ app/quasar.config.js
@@ -44,7 +44,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: process.env.BUILD_PUBLIC_PATH || '/',
+      publicPath: process.env.BUILD_PUBLIC_PATH || "/",
       // analyze: true,
       // env: {},
       // rawDefine: {}
